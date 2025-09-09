9b873202d69c8830823800fa48d7604935e7a5ba
gee <ngregline@gmail.com>
2025-09-09 13:43:17 -0400

    :wq

---
+++ codewars.py
@@ -0,0 +1,18 @@
+#Solving Even or Odd
+def even_or_odd(number):
+    if number%2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+#Converting integers to strings
+def number_to_string(num):
+    return str(num)
+
+#Removing Space
+def no_space(x):
+    return x.replace(" ", '')#your code here
+
+#Counting Vowels
+def no_space(x):
+    return x.replace(" ", '')#your code here
